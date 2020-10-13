bf3180718e7ab80f31088e4976210fe24c0b1284
Kara Caputo <kncaputo@gmail.com>
2020-10-13 16:01:30 -0400

    Test recipe constructors

--- src/recipe.js
+++ src/recipe.js
@@ -1,10 +1,24 @@
-const Ingredient = require('../src/ingredient');
+// const Ingredient = require('../src/ingredient');
 
 class Recipe {
-  constructor(id, imgSrc, name ) {
-    this.id = id;
-    this.image = imgSrc;
-    this.name = name;
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients;
+    this.name = recipe.name;
+    this.instructions = recipe.instructions;
+  }
+
+  calculateCost() {
+    // start with this.ingredients (array)
+    // this.ingredients[i] include id (#) & quantity object
+      // Grab this.ingredients[i].id
+    // in ingredients dataset, check for presence of id numbe in value of each object's //
+    // If they match, make a new empty array and push the cost into that
+  }
+
+  getInstructions() {
+
   }
 }
 
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -1,14 +1,49 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Ingredient = require('../src/ingredient');
+// const Ingredient = require('../src/ingredient');
 const Recipe = require('../src/recipe');
 
 describe('Recipe', () => {
   let recipe;
+  const sampleIngredientsData = [
+    {
+      "id": 1,
+      "name": "instant vanilla pudding",
+      "estimatedCostInCents": 660
+    },
+    {
+      "id": 2,
+      "name": "brown sugar",
+      "estimatedCostInCents": 559
+    }];
+
+  let pumpkinJuice = {id: 123, image: 'https://exampleimage.com/1/1/1', ingredients: [{
+    "id": 1,
+    "quantity": {
+      "amount": 2,
+      "unit": "c"
+    }
+  },
+  {
+    "id": 2,
+    "quantity": {
+      "amount": 1.5,
+      "unit": "tsp"
+    }
+  }],
+  name: 'Pumpkin Juice',
+  instructions: [{
+    "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+    "number": 1
+  },
+  {
+    "instruction": "Add egg and vanilla and mix until combined.",
+    "number": 2
+  }]};
 
   beforeEach(() => {
-    recipe = new Recipe(123, 'https://exampleimage.com/1/1/1', 'Pumpkin Juice');
+    recipe = new Recipe(pumpkinJuice);
   });
 
   it('should be a function', () => {
@@ -31,4 +66,23 @@ describe('Recipe', () => {
     expect(recipe.name).to.be.a('string');
     expect(recipe.name).to.deep.equal('Pumpkin Juice');
   });
+
+  it('should contain an array of ingredients', () => {
+    expect(recipe.ingredients).to.be.an('array');
+  });
+
+  it('should contain objects in the ingredients array', () => {
+    expect(recipe.ingredients[0]).to.be.an('object');
+    expect(recipe.ingredients[1]).to.be.an('object');
+  });
+
+  it('should have an id for each ingredient', () => {
+    expect(recipe.ingredients[0].id).to.deep.equal(1);
+    expect(recipe.ingredients[1].id).to.deep.equal(2);
+  });
+
+  it('should contain an array of instructions', () => {
+    expect(recipe.instructions).to.be.an('array');
+  });
+
 });
