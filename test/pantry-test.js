bb06b64db8a5b21af02132627ccd8f1ab67970c0
Bailey Dunning <bwdunning@gmail.com>
2020-10-13 17:53:01 -0600

    Complete test and constructor for pantry

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -5,12 +5,30 @@ const Pantry = require('../src/pantry');
 
 describe('Pantry', () => {
   let pantry;
+  let pantryItems = [{
+    "ingredient": 1,
+    "amount": 3
+  },
+  {
+    "ingredient": 2,
+    "amount": 7
+  }];
 
   beforeEach(() => {
-    pantry = new Pantry()
+    pantry = new Pantry();
   });
 
   it('should be a function', () => {
     expect(Pantry).to.be.a('function');
   });
+
+  it('should create an instance of pantry', () => {
+    expect(pantry).to.be.an.instanceof(Pantry);
+  });
+
+  it('should contain an array of items', () => {
+    expect(pantry.items).to.be.an('array');
+  });
+
+  
 });
