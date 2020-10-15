3edae37457362793763c6ed5e3222d6cdd59da9f
Bailey Dunning <bwdunning@gmail.com>
2020-10-15 14:43:20 -0600

    Add User class and test

---
+++ test/user-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Ingredient = require('../src/ingredient');
+const Recipe = require('../src/recipe');
+const Pantry = require('../src/pantry');
+const User = require('../src/user.js');
+
+describe('User', () => {
+  let user;
+
+  beforeEach(() => {
+    user = new User;
+  });
+
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+});
