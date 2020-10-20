b2aee2425219acc01a19e2ca4fb5288d5cb3157f
Kara Caputo <kncaputo@gmail.com>
2020-10-19 20:57:58 -0400

    General refactor of scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,7 @@
 // querySelectors
 let modal;
-const recipeCardContainer = document.querySelector('.recipes-container');
+const favoritesContainer = document.querySelector('.favorites-container');
+const whatsCookinContainer = document.querySelector('.whats-cookin-container');
 const myFavoritesNav = document.querySelector('#my-favorites-nav');
 const allRecipesNav = document.querySelector('#all-recipes-nav');
 const myPantryNav = document.querySelector('#my-pantry-nav');
@@ -27,58 +28,54 @@ allRecipesContainer.addEventListener('click', () => {
   determineClickOnAllRecipes(event);
 });
 
-recipeCardContainer.addEventListener('click', () => {
-  determineClick(event);
+favoritesContainer.addEventListener('click', () => {
+  determineClickInFavorites(event);
 });
-// ON HOMESCREEN
-//when heart is clicked
-// need to be able to mark as favorite/ unmark as favorite
-  // need eventlisteners for heart icons (through event bubbling)
-  // this should change boolean value, change heart icon (change heart icon)
 
-  //when plus is clicked
-  // need to be able to mark as readyToCook / unmark as readyToCook
-   // need eventlisteners for plus (through event bubbling)
-   // this should change boolean value of readyToCook, change plus icon
-// ----------------------
-// when favorites page is clicked on,
-  // parse all recipes,
-    // if isFavorites is true,
-      // create recipe card and display
-      // need eventlisteners for plus (through event bubbling)
-  // when heart is deselected,
-     // boolean value needs to change, toggle heart graphic, and splice html.
+whatsCookinContainer.addEventListener('click', () => {
+  determineClickInWhatsCookin(event);
+});
 
-//WHATS COOKIN
-//when what's Cookin page is clicked on,
-  // parse all recipes
-      // if what's cookin is true
-      // create recipe card and display
-      // need eventlisteners for plus (through event bubbling)
-// when plus icon is deselected (aka minus is clicked),
-  // boolean value needs to change, toggle plus graphic, and splice html.
-
-// function determineClick(event) {
-//   addRemoveFavorites(event);
-//   addToWhatsCookin(event);
-//   openModel(event);
-// }
 function determineClickOnAllRecipes(event) {
   markUnmarkAsFavorite(event);
   markUnmarkReadyToCook(event);
+  openModal(event);
 }
 
-function determineClick(event) {
-  markUnmarkAsFavorite(event);
+function determineClickInFavorites(event) {
+  recipeBox.allRecipes.forEach(recipe => {
+    if (event.target.id === `favorite-btn-${recipe.id}`) {
+      markUnmarkAsFavorite(event);
+      removeRecipeCard(event);
+    }
+  })
   markUnmarkReadyToCook(event);
-  removeRecipeCard(event);
-  openModel(event);
+  openModal(event);
 }
+
+function determineClickInWhatsCookin(event) {
+  recipeBox.allRecipes.forEach(recipe => {
+    if (event.target.id === `whats-cookin-btn-${recipe.id}`) {
+      markUnmarkReadyToCook(event);
+      removeRecipeCard(event);
+    }
+  })
+  markUnmarkAsFavorite(event);
+  openModal(event);
+}
+
+// if (event.target.id === ) {
+//   markUnmarkReadyToCook(event);
+//   // debugger
+//   removeRecipeCard(event);
+// }
+// // removeRecipeCard(event);
 
 function markUnmarkAsFavorite(event) {
   recipeBox.allRecipes.forEach(recipe => {
     if (event.target.id === `favorite-btn-${recipe.id}`) {
       updateRecipeBoolean(recipe, 'isFavorite');
+      // debugger
       toggleHeartImg(recipe);
     }
   })
@@ -115,36 +112,6 @@ function displayAllRecipes(recipes) {
   })
 }
 
-// function displaySavedRecipes(recipes) {
-//   recipes.forEach(recipe => {
-//     let recipeCard = createRecipes(recipe)
-//     recipeCardContainer.insertAdjacentHTML('afterbegin', recipeCard);
-//   })
-// }
-//
-// function highlightPageOnMenu(id) {
-//   let navButton = document.getElementById(id);
-//
-//   if (navButton.style.color === 'black') {
-//     navButton.style.color = '#d54215';
-//   } else {
-//     navButton.style.color = 'black';
-//   }
-// }
-
-//
-// function highlightPageOnMenu(eventId) {
-//   let navButton = document.getElementById(eventId);
-//
-//   const navButtons = ['nav1', 'nav2', 'nav3', 'nav4'];
-//   navButtons.forEach(id => {
-//     if (navId === eventId) {
-//       id.style.color = '#145b9c'
-//     } else {
-//       id.style.color = '#d54215'
-//   })
-// }
-
   function createRecipes(recipe) {
   let recipeCard = `<div class="recipe-card recipe-${recipe.id}" id="recipe-${recipe.id}">
     <div class="recipe-img-box">
@@ -152,7 +119,7 @@ function displayAllRecipes(recipes) {
     </div>
     <div class="recipe-action-btns flex-row">
       <button id="favorite-btn"><img src="../assets/heart-icon-${recipe.isFavorite}.png" id="favorite-btn-${recipe.id}" alt="favorite button"></button>
-      <button id="whats-cookin-btn"><img src="../assets/plus-icon-${recipe.readyToCook}.png" id="whats-cookin-btn-${recipe.id}" alt="save button"></button>
+      <button class="whats-cookin-btn" id="whats-cookin-btn"><img class="whats-cookin-btn" src="../assets/plus-icon-${recipe.readyToCook}.png" id="whats-cookin-btn-${recipe.id}" alt="save button"></button>
     </div>
     <h3>${recipe.name}</h3>
     <button class="show-recipe-btn-${recipe.id}" id="show-recipe-btn"><h3 class="show-recipe-btn-${recipe.id}">Show Recipe</h3></button>
@@ -201,107 +168,64 @@ function displayIngredients() {
   });
 }
 
+
 function showAllRecipes() {
   allRecipesContainer.classList.remove('hidden');
   searchContainer.classList.remove('hidden');
-  recipeCardContainer.innerHTML = '';
+  favoritesContainer.innerHTML = '';
   pantryContainer.innerHTML = '';
+  whatsCookinContainer.innerHTML = '';
   displayAllRecipes(recipeBox.allRecipes);
-  // displayAllRecipes(user.favoriteRecipes);
-  // displayAllRecipes(user.recipesToCook);
-  highlightPageOnMenu('nav1');
+  // highlightPageOnMenu('nav1');
 }
 
 function showFavorites() {
   allRecipesContainer.classList.add('hidden');
   searchContainer.classList.remove('hidden');
-  recipeCardContainer.innerHTML = '';
+  favoritesContainer.innerHTML = '';
   pantryContainer.innerHTML = '';
+  whatsCookinContainer.innerHTML = '';
+
   recipeBox.allRecipes.forEach(recipe => {
     if (recipe.isFavorite === true) {
-      displayRecipesInFavorites(recipe);
+      displaySavedRecipes(recipe, 'favoritesContainer');
     }
   })
 }
 
-function displayRecipesInFavorites(recipe) {
+function displaySavedRecipes(recipe, container) {
   let recipeCard = createRecipes(recipe)
-  recipeCardContainer.insertAdjacentHTML('afterbegin', recipeCard);
+  if (container === 'favoritesContainer') {
+    favoritesContainer.insertAdjacentHTML('afterbegin', recipeCard);
+  } else if (container === 'whatsCookinContainer') {
+    whatsCookinContainer.insertAdjacentHTML('afterbegin', recipeCard);
+  }
 }
 
+// function displaySavedRecipes(recipe) {
+//   let recipeCard = createRecipes(recipe)
+//   recipeCardContainer.insertAdjacentHTML('afterbegin', recipeCard);
+// }
+
 function showMyPantry() {
-  allRecipesContainer.classList.remove('hidden');
+  allRecipesContainer.classList.add('hidden');
   searchContainer.classList.remove('hidden');
-  recipeCardContainer.innerHTML = '';
+  favoritesContainer.innerHTML = '';
+  pantryContainer.innerHTML = '';
+  whatsCookinContainer.innerHTML = '';
   displayIngredients();
-  highlightPageOnMenu('nav3');
+  // highlightPageOnMenu('nav3');
 }
 
 function showWhatsCookin() {
+  allRecipesContainer.classList.add('hidden');
   searchContainer.classList.add('hidden');
-  recipeCardContainer.innerHTML = '';
+  favoritesContainer.innerHTML = '';
   pantryContainer.innerHTML = '';
-  displaySavedRecipes(user.recipesToCook);
-  highlightPageOnMenu('nav4');
-}
-
-// function addRecipeToFavorites(target) {
-//   recipeBox.allRecipes.forEach(recipe => {
-//     if (event.target.id === `favorite-btn-${recipe.id}`) {
-//       console.log(`added ${recipe.name} to favorites`)
-//       user.toggleRecipeStatus(user.favoriteRecipes, 'isFavorite', recipe);
-//     }
-//   })
-// }
-//
-// function addRecipeToWhatsCookin(target) {
-//   recipeBox.allRecipes.forEach(recipe => {
-//     if (event.target.id === `whats-cookin-btn-${recipe.id}`) {
-//       console.log(`added ${recipe.name} to whats cookin`)
-//       user.toggleRecipeStatus(user.recipesToCook, 'readyToCook', recipe);
-//     }
-//   })
-// }
-
-// -------------- NOTES -----------------
-// under user remove favorites arr and ready to cook array
-// use the booleans in recipe when displaying favorites and ready to cook
-
-//
-// function addRemoveFavorites(target) {
-//   let recipe = findRecipeFromEvent(event)
-//   recipeBox.allRecipes.forEach(recipe => {
-//
-//     if ((event.target.id === `favorite-btn-${recipe.id}`) && (recipe.isFavorite === false)) {
-//
-//       console.log(`added ${recipe.name} to favorites`)
-//       user.toggleRecipeStatus(user.favoriteRecipes, 'isFavorite', recipe);
-//       toggleHeartImg(recipe);
-//       debugger
-//     } else if ((event.target.id === `favorite-btn-${recipe.id}`) && (recipe.isFavorite === true)){
-//       console.log(`removed ${recipe.name} from favorites`)
-//       //needs to make isFavorite = false (This is done on line 23 of toggleRecipeStatus)
-//       //heart will need to be toggled on recipe in all recipes container
-//       toggleHeartImg(recipe);
-//       removeRecipeCard(recipe);
-//
-//       user.toggleRecipeStatus(user.favoriteRecipes, 'isFavorite', recipe);
-//       // removeRecipeCard(recipe);
-//       // recipeBox.allRecipes.push(recipe);
-//     }
-//   })
-// }
-
-function addRemoveWhatsCookin(target) {
+  whatsCookinContainer.innerHTML = '';
   recipeBox.allRecipes.forEach(recipe => {
-    if ((event.target.id === `whats-cookin-btn-${recipe.id}`) && (recipe.readyToCook === false)) {
-      console.log(`added ${recipe.name} to What's Cookin'`)
-      user.toggleRecipeStatus(user.readyToCook, 'readyToCook', recipe);
-      togglePlusImg(recipe);
-    } else if ((event.target.id === `whats-cookin-btn-${recipe.id}`) && (recipe.readyToCook === true)) {
-      user.toggleRecipeStatus(user.readyToCook, 'readyToCook', recipe);
-      removeRecipeCard(recipe);
-      // recipeBox.allRecipes.push(recipe);
+    if (recipe.readyToCook === true) {
+      displaySavedRecipes(recipe, 'whatsCookinContainer');
     }
   })
 }
@@ -309,10 +233,10 @@ function addRemoveWhatsCookin(target) {
 function removeRecipeCard(event) {
   recipeBox.allRecipes.forEach(recipe => {
     if (event.target.id === `favorite-btn-${recipe.id}`) {
-      let toRemove = document.querySelector(`.recipes-container .recipe-${recipe.id}`);
+      let toRemove = document.querySelector(`.favorites-container .recipe-${recipe.id}`);
       toRemove.remove(event.target.id === `favorite-btn-${recipe.id}`);
     } else if (event.target.id === `whats-cookin-btn-${recipe.id}`) {
-      let toRemove = document.querySelector(`.recipes-container .recipe-${recipe.id}`);
+      let toRemove = document.querySelector(`.whats-cookin-container .recipe-${recipe.id}`);
       toRemove.remove(event.target.id === `whats-cookin-btn-${recipe.id}`);
     }
   })
@@ -328,9 +252,9 @@ function toggleHeartImg(recipe) {
 
 function togglePlusImg(recipe) {
   if (recipe.readyToCook === true) {
-    document.querySelector(`#whats-cookin-btn-${recipe.id}`).src = `../assets/plus-icon-${recipe.readyToCook}.png`
+    document.querySelector(`#whats-cookin-btn-${recipe.id}`).src = `../assets/plus-icon-true.png`
   } else {
-    document.querySelector(`#whats-cookin-btn-${recipe.id}`).src = `../assets/plus-icon-${recipe.readyToCook}.png`
+    document.querySelector(`#whats-cookin-btn-${recipe.id}`).src = `../assets/plus-icon-false.png`
   }
 }
 
@@ -343,7 +267,7 @@ function addToWhatsCookin(target) {
   })
 }
 
-function openModel() {
+function openModal() {
   recipeBox.allRecipes.forEach(recipe => {
     if (event.target.className === `show-recipe-btn-${recipe.id}`) {
       modal = document.querySelector('.modal');
